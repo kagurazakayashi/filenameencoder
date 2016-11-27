acf2a6e83abedf074cd3fc84fbe71250b099eda2
神楽坂雅詩 <cyscxchope@gmail.com>
2016-11-27 15:29:00 +0800

    更新帮助,修正一些显示问题
    Enter "filenameencoder.py --help" to get help.

---
+++ README.md
@@ -0,0 +1,90 @@
+## Filenameencoder
+
+非英文字符文件名.jpg -> Filenameencoder rename -> 6Z2e6Iux5paH5a2X56ym5paH5Lu25ZCN.jpg -> Filenameencoder rename -> 非英文字符文件名.jpg
+
+###Usage:
+
+filenameencoder.py \[\[--encoding [disable|base64|md5] | --decoding [disable|base64]] \[--file "filename" | --folder "foldername"] \[--readonly] \[--hiddenfiles] \[--yes] \[--readencode [auto|utf-8|mbcs|gbk|...]] [--writeencode [auto|utf-8|mbcs|gbk|...]]
+
+Enter "filenameencoder.py --help" to get help.
+
+-  --help [en|cn] | -h [...] | /? [...]:
+-    Display the help. Default value is en.
+-  --encode [disable|base64|md5] | -e [...] | /e [...] :
+-    Set encoding mode (default). Default value is disable.
+-  --decode [disable|base64] | -d [...] | /d [...] :
+-    Set decoding mode. Default value is disable.
+-  --file "filename" | -i "filename" | /i "filename" :
+-    Rename a file.
+-  --folder "foldername" | -f "foldername" | /f "foldername" :
+-    Rename all files in a folder.
+-  --hiddenfiles | -s | /s :
+-    Include hidden files. Default value is void (not included).
+-  --yes | -y | /y :
+-    No confirmation will follow. Default value is void (no).
+-  --readencode [auto|utf-8|mbcs|gbk|...] | -r [...] | /r [...] :
+-    (Advanced) Read file name using encoding. Default value is auto.
+-  --writeencode [auto|utf-8|mbcs|gbk|...] | -w [...] | /w [...] :
+-    (Advanced) Write file name using encoding. Default value is auto.
+
+###使用方法:
+
+filenameencoder.py \[\[--encoding [disable或base64或md5]或者--decoding [disable或base64]] \[--file "文件名"或者--folder "文件夹名"] \[--readonly] \[--hiddenfiles] \[--yes] \[--readencode [auto或utf-8或mbcs或gbk或...]] [--writeencode [auto或utf-8或mbcs或gbk或...]]
+
+-  --help [en或cn] 或者 -h [...] 或者 /? [...] :
+-    显示这些帮助信息，添加 cn 可以显示此中文帮助。默认值为英语。
+-  --encode [disable或base64或md5] 或者 -e [...] 或者 /e [...] :
+-    使用指定方式编码（默认）。默认值是 disable 。
+-  --decode [disable或base64] 或者 -d [...] 或者 /d [...] :
+-    使用指定方式解码。默认值是 disable 。
+-  --file "文件名" 或者 -i "文件名" /i "文件名" :
+-    重命名单一文件。
+-  --folder "文件夹名" 或者 -f "文件夹名" 或者 /f "文件夹名" :
+-    重命名一个文件夹中的所有文件。
+-  --hiddenfiles 或者 -s 或者 /s :
+-    包含隐藏文件。默认值是不包含隐藏文件。
+-  --yes 或者 -y 或者 /y :
+-    不进行确认询问，直接进行重命名操作。默认值是需要询问。
+-  --readencode [auto或utf-8或mbcs或gbk或...] 或者 -r [...] 或者 /r [...] :
+-    (高级选项)指定读取文件名时使用的字符编码。默认值为自动。
+-  --writeencode [auto或utf-8或mbcs或gbk或...] 或者 -w [...] 或者 /w [...] :
+-    (高级选项)指定写入文件名时使用的字符编码。默认值为自动。
+
+###e.g. / 使用示范 :
+
+- command$ python filenameencoder.py -e base64 -f test/
+- *. /Users/KagurazakaYashi/filenameencoder/test/.DS_Store
+- -> Skip hidden file.
+- 1. /Users/KagurazakaYashi/filenameencoder/test/重命名测试文件1.test
+- -> /Users/KagurazakaYashi/filenameencoder/test/5rWL6K+V5pWw5o2uMQ==.test
+- 2. /Users/KagurazakaYashi/filenameencoder/test/重命名测试文件2.test
+- -> /Users/KagurazakaYashi/filenameencoder/test/5rWL6K+V5pWw5o2uMg==.test
+- 2 Ready, 1 Skip, 3 Total
+- Start rename (y/N)? :y
+- Start rename ...
+- 1. /Users/KagurazakaYashi/filenameencoder/test/重命名测试文件1.test
+- -> /Users/KagurazakaYashi/filenameencoder/test/5rWL6K+V5pWw5o2uMQ==.test
+- -> OK.
+- 2. /Users/KagurazakaYashi/filenameencoder/test/重命名测试文件2.test
+- -> /Users/KagurazakaYashi/filenameencoder/test/5rWL6K+V5pWw5o2uMg==.test
+- -> OK.
+- 2 OK, 0 Fail, 1 Skip, 3 Total.
+
+
+- command$ python filenameencoder.py -d base64 -f test/
+- *. /Users/KagurazakaYashi/filenameencoder/test/.DS_Store
+- -> Skip hidden file.
+- 1. /Users/KagurazakaYashi/filenameencoder/test/5rWL6K+V5pWw5o2uMg==.test
+- -> /Users/KagurazakaYashi/filenameencoder/test/重命名测试文件2.test
+- 2. /Users/KagurazakaYashi/filenameencoder/test/5rWL6K+V5pWw5o2uMQ==.test
+- -> /Users/KagurazakaYashi/filenameencoder/test/重命名测试文件1.test
+- 2 Ready, 1 Skip, 3 Total
+- Start rename (y/N)? :y
+- Start rename ...
+- 1. /Users/KagurazakaYashi/filenameencoder/test/5rWL6K+V5pWw5o2uMg==.test
+- -> /Users/KagurazakaYashi/filenameencoder/test/重命名测试文件2.test
+- -> OK.
+- 2. /Users/KagurazakaYashi/filenameencoder/test/5rWL6K+V5pWw5o2uMQ==.test
+- -> /Users/KagurazakaYashi/filenameencoder/test/重命名测试文件1.test
+- -> OK.
+- 2 OK, 0 Fail, 1 Skip, 3 Total.
--- filenameencoder.py
+++ filenameencoder.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-#KagurazakaYashi
+# KagurazakaYashi
+# 非英文字符文件名.jpg -> Filenameencoder rename -> 6Z2e6Iux5paH5a2X56ym5paH5Lu25ZCN.jpg -> Filenameencoder rename -> 非英文字符文件名.jpg
 import sys
 import os
 import os.path
@@ -23,13 +24,17 @@ class Filenameencoder:
         print "\nYashi Filenameencoder v1.2 for py2" #,sys.argv[0]
         #for i in range(1, len(sys.argv)):
             #print "parameter", i, sys.argv[i]
+    def minihelp(self):
+        print "\n  usage: "+sys.argv[0]+" [[--encoding [disable|base64|md5] | --decoding [disable|base64]] [--file <filename> | --folder <foldername>] [--readonly] [--hiddenfiles] [--yes] [--readencode [auto|utf-8|mbcs|gbk|...]] [--writeencode [auto|utf-8|mbcs|gbk|...]]\n"
+        print "  Enter \""+sys.argv[0]+" --help\" to get help."
+        print "  --help [en|cn] | -h [en|cn] | /? [en|cn]:\n"
     #显示英文帮助信息
     def help(self):
         hlp = [
-        "usage: "+sys.argv[0]+" [--readencode [auto|utf-8|mbcs|gbk|...]] [--writeencode [auto|utf-8|mbcs|gbk|...]] [[--encoding [disable|base64|md5] | --decoding [disable|base64]] [--file <filename> | --folder <foldername>] [--readonly] [--hiddenfiles] [--yes]",
-        "--help [en|cn] | -h [en|cn] | /? [en|cn]:",
+        "\nusage: "+sys.argv[0]+" [[--encoding [disable|base64|md5] | --decoding [disable|base64]] [--file <filename> | --folder <foldername>] [--readonly] [--hiddenfiles] [--yes] [--readencode [auto|utf-8|mbcs|gbk|...]] [--writeencode [auto|utf-8|mbcs|gbk|...]]\n",
+        "--help [en|cn] | -h [...] | /? [...]:",
         "  Display the help. Default value is en.",
-        "--encode [disable|base64|md5] | -e [...]  | /e [...] :",
+        "--encode [disable|base64|md5] | -e [...] | /e [...] :",
         "  Set encoding mode (default). Default value is disable.",
         "--decode [disable|base64] | -d [...] | /d [...] :",
         "  Set decoding mode. Default value is disable.",
@@ -43,22 +48,19 @@ class Filenameencoder:
         "  No confirmation will follow. Default value is void (no).",
         "--readencode [auto|utf-8|mbcs|gbk|...] | -r [...] | /r [...] :",
         "  (Advanced) Read file name using encoding. Default value is auto.",
-        "--writeencode [auto|utf-8|mbcs|gbk|...] | -w | /w :",
+        "--writeencode [auto|utf-8|mbcs|gbk|...] | -w [...] | /w [...] :",
         "  (Advanced) Write file name using encoding. Default value is auto.",
-        " "]
-        for i in range(0, len(hlp)):
-            codestr = self.strencode("  "+hlp[i],"utf-8",self.systemencoding)
-            if codestr != None:
-                print codestr
+        "\ne.g. :"]
+        self.helpview(hlp+self.helpdemo(),"Press Enter to continue")
     #显示中文帮助信息
     def helpcn(self):
         hlp = [
-        "使用方法: "+sys.argv[0]+" [--readencode [auto|utf-8|mbcs|gbk|...]] [--writeencode [auto|utf-8|mbcs|gbk|...]] [[--encoding [disable|base64|md5] | --decoding [disable|base64]] [--file <文件名> | --folder <文件夹名>] [--readonly] [--hiddenfiles] [--yes]",
-        "--help [en|cn] 或者 -h [en|cn] 或者 /? [en|cn]:",
+        "\n使用方法: "+sys.argv[0]+" [[--encoding [disable或base64或md5]或者--decoding [disable或base64]] [--file <文件名>或者--folder <文件夹名>] [--readonly] [--hiddenfiles] [--yes] [--readencode [auto或utf-8或mbcs或gbk或...]] [--writeencode [auto或utf-8或mbcs或gbk或...]]\n",
+        "--help [en或cn] 或者 -h [...] 或者 /? [...] :",
         "  显示这些帮助信息，添加 cn 可以显示此中文帮助。默认值为英语。",
-        "--encode [disable|base64|md5] 或者 -e [...] 或者 /e [...] :",
+        "--encode [disable或base64或md5] 或者 -e [...] 或者 /e [...] :",
         "  使用指定方式编码（默认）。默认值是 disable 。",
-        "--decode [disable|base64] 或者 -d [...] 或者 /d [...] :",
+        "--decode [disable或base64] 或者 -d [...] 或者 /d [...] :",
         "  使用指定方式解码。默认值是 disable 。",
         "--file <文件名> 或者 -i <文件名> /i <文件名> :",
         "  重命名单一文件。",
@@ -68,12 +70,65 @@ class Filenameencoder:
         "  包含隐藏文件。默认值是不包含隐藏文件。",
         "--yes 或者 -y 或者 /y :",
         "  不进行确认询问，直接进行重命名操作。默认值是需要询问。",
-        "--readencode [auto|utf-8|mbcs|gbk|...] | -r [...] | /r [...] :",
+        "--readencode [auto或utf-8或mbcs或gbk或...] 或者 -r [...] 或者 /r [...] :",
         "  (高级选项)指定读取文件名时使用的字符编码。默认值为自动。",
-        "--writeencode [auto|utf-8|mbcs|gbk|...] | -w [...] | /w [...] :",
+        "--writeencode [auto或utf-8或mbcs或gbk或...] 或者 -w [...] 或者 /w [...] :",
         "  (高级选项)指定写入文件名时使用的字符编码。默认值为自动。",
-        " "]
+        "\n使用示范："]
+        self.helpview(hlp+self.helpdemo(),"按回车键继续")
+    def helpdemo(self):
+        t1 = "/Users/KagurazakaYashi/filenameencoder/test/"
+        t2 = "-> "
+        t3 = "重命名测试文件"
+        t4 = ".test"
+        t5 = "5rWL6K+V5pWw5o2uMQ=="
+        t6 = "5rWL6K+V5pWw5o2uMg=="
+        t7 = "$"
+        if 'Windows' in platform.system():
+            t7 = ">"
+        t7 = "command"+t7+" python "+sys.argv[0]+" "
+        dmo = [
+            t7+"-e base64 -f test/",
+            "*. "+t1+".DS_Store",
+            t2+"Skip hidden file.",
+            "1. "+t1+t3+"1"+t4,
+            t2+t1+t5+t4,
+            "2. "+t1+t3+"2"+t4,
+            t2+t1+t6+t4,
+            "2 Ready, 1 Skip, 3 Total",
+            "Start rename (y/N)? :y",
+            "Start rename ...",
+            "1. "+t1+t3+"1"+t4,
+            t2+t1+t5+t4,
+            t2+"OK.",
+            "2. "+t1+t3+"2"+t4,
+            t2+t1+t6+t4,
+            t2+"OK.",
+            "2 OK, 0 Fail, 1 Skip, 3 Total.\n",
+            t7+"-d base64 -f test/",
+            "*. "+t1+".DS_Store",
+            t2+"Skip hidden file.",
+            "1. "+t1+t6+t4,
+            t2+t1+t3+"2"+t4,
+            "2. "+t1+t5+t4,
+            t2+t1+t3+"1"+t4,
+            "2 Ready, 1 Skip, 3 Total",
+            "Start rename (y/N)? :y",
+            "Start rename ...",
+            "1. "+t1+t6+t4,
+            t2+t1+t3+"2"+t4,
+            t2+"OK.",
+            "2. "+t1+t5+t4,
+            t2+t1+t3+"1"+t4,
+            t2+"OK.",
+            "2 OK, 0 Fail, 1 Skip, 3 Total."]
+        for i in range(0, len(dmo)):
+            dmo[i] = "| "+dmo[i]
+        return dmo
+    def helpview(self,hlp,pause):
         for i in range(0, len(hlp)):
+            if i%15 == 0 and i != 0:
+                content = raw_input("["+pause+"]")
             codestr = self.strencode("  "+hlp[i],"utf-8",self.systemencoding)
             if codestr != None:
                 print codestr
@@ -207,7 +262,7 @@ class Filenameencoder:
     #参数错误
     def argumenterr(self):
         print "  No parameter or parameter error."
-        self.help()
+        self.minihelp()
     #转换开始
     def filenamepreview(self):
         renamep = []
@@ -235,7 +290,7 @@ class Filenameencoder:
             elif cfilename == "MD":
                 print "[ERROR] Irreversible algorithm."
                 return False
-            oldp = os.path.join(dir,filename+extname)
+            oldp = os.path.join(path[0],path[1]+path[2])
             newp = os.path.join(dir,cfilename+extname)
             if self.hiddenfile == False and self.isHidenFile(oldp,filename) == True:
                 print "*.",oldp
